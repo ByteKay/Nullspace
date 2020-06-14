045b37d5553dd29b7ba13a2bb211983e70e349ea
bytekay <1025115216@qq.com>
2020-06-14 20:19:55 +0800

    modify

--- Assets/Scripts/BVHTree/Utils/GeoUtils.cs
+++ Assets/Scripts/BVHTree/Utils/GeoUtils.cs
@@ -16,6 +16,97 @@ namespace Nullspace
             return new Vector2(v.x, v.y);
         }
 
+        /// <summary>
+        /// 根据路点生成 NavPath
+        /// </summary>
+        /// <param name="waypoints">路点</param>
+        /// <param name="subdivisions">两个路点之间的分段数量</param>
+        /// <returns></returns>
+        public static NavPathData CreatePathData(List<Vector3> waypoints, int subdivisions = 5)
+        {
+            NavPathData pathData = new NavPathData();
+            List<Vector3> wayPoints = new List<Vector3>();
+            foreach (Vector3 v in waypoints)
+            {
+                wayPoints.Add(new Vector3(v.x, v.y, v.z));
+            }
+            int cnt = wayPoints.Count;
+            pathData.OriginWayPoints = new List<Vector3>();
+            pathData.OriginWayPoints.AddRange(wayPoints);
+            if (cnt < 2)
+            {
+                Debug.LogError("RFPathData.CreatePathData 路点数据量不够 < 2");
+                return pathData;
+            }
+            if (cnt == 2)
+            {
+                // 直接用直线, 直接加
+                // 后面可通过 判断多点共线，直接使用支线
+                pathData.WayPoints.AddRange(wayPoints);
+            }
+            else
+            {
+                float div = 1.0f / subdivisions;
+                float half = 0.5f * div;
+                List<Vector3> temp = new List<Vector3>();
+                Vector3 diff = (2.0f * wayPoints[1] - wayPoints[0] - wayPoints[2]) * 0.5f;
+                temp.Add(wayPoints[0] + diff);
+                temp.AddRange(wayPoints);
+
+                diff = (2.0f * wayPoints[wayPoints.Count - 2] - wayPoints[wayPoints.Count - 3] - wayPoints[wayPoints.Count - 1]) * 0.5f;
+                temp.Add(wayPoints[wayPoints.Count - 1] + diff);
+                cnt = temp.Count - 2;
+                // path.KeyPoints 的第0号，现在是第1号；最后一个，现在是倒数第2个
+                for (int i = 1; i < cnt; ++i)
+                {
+                    for (float u = 0; u < 1 - div * half; u += div)
+                    {
+                        Vector3 inter = .5f * (
+                           (-temp[i - 1] + 3f * temp[i] - 3f * temp[i + 1] + temp[i + 2]) * (u * u * u)
+                           + (2f * temp[i - 1] - 5f * temp[i] + 4f * temp[i + 1] - temp[i + 2]) * (u * u)
+                           + (-temp[i - 1] + temp[i + 1]) * u
+                           + 2f * temp[i]);
+                        pathData.WayPoints.Add(inter);
+                    }
+                }
+                pathData.WayPoints.Add(wayPoints[wayPoints.Count - 1]);
+            }
+            cnt = pathData.WayPoints.Count;
+            pathData.pathLength = 0.0f;
+            pathData.RangeLengths.Add(0.0f);
+            for (int i = 1; i < cnt; ++i)
+            {
+                Vector3 diff = pathData.WayPoints[i] - pathData.WayPoints[i - 1];
+                float length = diff.magnitude;
+                pathData.pathLength += length;
+                pathData.RangeLengths.Add(pathData.pathLength);
+            }
+            return pathData;
+        }
+
+        public static float Interpolation(float left, float right, float t)
+        {
+            bool needRevert = left > right;
+
+            if (needRevert)
+            {
+                left = -left;
+                right = -right;
+            }
+
+            if (t < 0) t = 0;
+            else if (t > 1) t = 1;
+
+            float v = left + t * (right - left);
+            return needRevert ? -v : v;
+        }
+        public static Vector3 Interpolation(Vector3 left, Vector3 right, float t)
+        {
+            if (t < 0) t = 0;
+            else if (t > 1) t = 1;
+            return left + t * (right - left);
+        }
+
         public static GeoAABB3 GetAABB(List<Vector3> points)
         {
             GeoAABB3 aabb = new GeoAABB3();
