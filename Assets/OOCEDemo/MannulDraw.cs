dabb3ed55f51cb215db181ac67417604d01ec6d1
bytekay <1025115216@qq.com>
2020-06-30 17:39:10 +0800

    使用 Graphics 接口 渲染

---
+++ Assets/OOCEDemo/MannulDraw.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Nullspace
+{
+    public class MannulDraw : MonoBehaviour
+    {
+        // DrawMesh(Mesh mesh, Matrix4x4 matrix, Material material, int layer);
+        private Mesh mesh;
+        private Material material;
+        private int layer;
+
+        private void Awake()
+        {
+            mesh = GetComponent<MeshFilter>().sharedMesh;
+            material = GetComponent<MeshRenderer>().material;
+        }
+
+        public void DrawMesh()
+        {
+            Graphics.DrawMesh(mesh, transform.localToWorldMatrix, material, gameObject.layer);
+        }
+
+        private void OnEnable()
+        {
+            MannulDrawManager.Instance.AddObject(this);
+        }
+
+        private void OnDisable()
+        {
+            MannulDrawManager.Instance.RemoveObject(this);
+        }
+
+        private void OnDestroy()
+        {
+            MannulDrawManager.Instance.RemoveObject(this);
+        }
+    }
+
+}
---
+++ Assets/OOCEDemo/MannulDrawManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Nullspace
+{
+
+    public class MannulDrawManager :  Singleton<MannulDrawManager>
+    {
+        private Dictionary<int, MannulDraw> DrawObjects;
+        protected virtual void Awake()
+        {
+            DrawObjects = new Dictionary<int, MannulDraw>();
+        }
+
+        public void AddObject(MannulDraw obj)
+        {
+            if (!DrawObjects.ContainsKey(obj.GetInstanceID()))
+            {
+                DrawObjects.Add(obj.GetInstanceID(), obj);
+            }
+        }
+
+        public void RemoveObject(MannulDraw obj)
+        {
+            if (DrawObjects.ContainsKey(obj.GetInstanceID()))
+            {
+                DrawObjects.Remove(obj.GetInstanceID());
+            }
+        }
+
+        public void Update()
+        {
+            foreach (MannulDraw draw in DrawObjects.Values)
+            {
+                draw.DrawMesh();
+            }
+        }
+
+        protected override void OnDestroy()
+        {
+            DrawObjects.Clear();
+        }
+    }
+}
+
+
