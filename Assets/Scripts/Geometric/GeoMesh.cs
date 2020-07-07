7c271efbb329307d00f78f25da55c77118617505
kayoyang <kayoyang@tencent.com>
2020-07-07 20:14:48 +0800

    删除

--- Assets/Scripts/Geometric/GeoMesh.cs
+++ Assets/Scripts/Geometric/GeoMesh.cs
@@ -1,16 +1,5 @@
-﻿/********************************************************************************
-** All rights reserved
-** Auth： kay.yang
-** E-mail: 1025115216@qq.com
-** Date： 6/30/2017 11:13:04 AM
-** Version:  v1.0.0
-*********************************************************************************/
-
-using System;
+﻿
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
 
 namespace Nullspace
 {
