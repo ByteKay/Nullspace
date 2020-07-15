fa8d0aa9336edbba9e449cfae642adbe8ed8e6d2
bytekay <1025115216@qq.com>
2020-07-15 20:00:09 +0800

    excel 转 cs 添加 IStream

--- Tools/ExcelToXml/XlsxConvert.cs
+++ Tools/ExcelToXml/XlsxConvert.cs
@@ -48,7 +48,6 @@ namespace Nullspace
                 {
                     continue;
                 }
-
                 GeneratorCS(file);
                 ExcelToXml(file);
             }
@@ -64,7 +63,36 @@ namespace Nullspace
             }
         }
 
-        public static void GeneratorCS(string fileFullPath, string sheetName = "Sheet1", int colName = 1, int colType = 3)
+        private static void ExportOneSheet(ExcelWorksheet tDS, int colName, int colType, ref List<string> names,  ref List<string> nameTypes)
+        {
+            ExcelRange range = tDS.Cells;
+            object[,] values = (object[,])range.Value;
+            int rows = values.GetLength(0);
+            int cols = values.GetLength(1);
+            int nameRow = colName - 1;
+            for (int j = 0; j < cols; ++j)
+            {
+                if (values[nameRow, j] == null)
+                {
+                    break;
+                }
+                string name = values[nameRow, j].ToString();
+                names.Add(name.ToString());
+            }
+            int nameTypeRow = colType - 1;
+            for (int j = 0; j < names.Count; ++j)
+            {
+                if (values[nameTypeRow, j] == null)
+                {
+                    values[nameTypeRow, j] = "";
+                    return;
+                }
+                string name = values[nameTypeRow, j].ToString();
+                nameTypes.Add(name.ToString());
+            }
+        }
+
+        public static void GeneratorCS(string fileFullPath, int colName = 1, int colType = 3)
         {
             FileInfo newFile = new FileInfo(fileFullPath);
             string fileName = Path.GetFileNameWithoutExtension(fileFullPath);
@@ -78,35 +106,13 @@ namespace Nullspace
                     ExcelWorkbook workBook = pck.Workbook;
                     if (workBook != null)
                     {
-                        if (workBook.Worksheets.Count > 0)
+                        int cnt = workBook.Worksheets.Count;
+                        var enumerator = workBook.Worksheets.GetEnumerator();
+                        while (enumerator.MoveNext())
                         {
-                            ExcelWorksheet tDS = workBook.Worksheets[sheetName];
-                            ExcelRange range = tDS.Cells;
-                            object[,] values = (object[,])range.Value;
-                            int rows = values.GetLength(0);
-                            int cols = values.GetLength(1);
-                            int nameRow = colName - 1;
-                            for (int j = 0; j < cols; ++j)
-                            {
-                                if (values[nameRow, j] == null)
-                                {
-                                    break;
-                                }
-                                string name = values[nameRow, j].ToString();
-                                names.Add(name.ToString());
-                            }
-                            int nameTypeRow = colType - 1;
-                            for (int j = 0; j < names.Count; ++j)
-                            {
-                                if (values[nameTypeRow, j] == null)
-                                {
-                                    values[nameTypeRow, j] = "";
-                                    Console.WriteLine("error type: " + fileFullPath);
-                                    return;
-                                }
-                                string name = values[nameTypeRow, j].ToString();
-                                nameTypes.Add(name.ToString());
-                            }
+                            ExcelWorksheet tDS = enumerator.Current;
+                            string sheetName = tDS.Name;
+                            ExportOneSheet(tDS, colName, colType, ref names, ref nameTypes);
                         }
                     }
                 }
@@ -145,12 +151,13 @@ namespace Nullspace
                 File.WriteAllText(filePath, builder.ToString());
             }
         }
+
         static void MakeCS(string fileName, string desc, List<string> names, List<string> nameTypes, StringBuilder builder, HashSet<string> removeField)
         {
             string tab = "    ";
             string doubleTab = tab + tab;
             builder.Append(tab).Append("[XmlData(\"").Append(fileName + ".xml\"").Append(", \"").Append(desc).Append("\")]").AppendLine("");
-            builder.Append(tab).Append("public class ").Append(fileName).Append(" : XmlData<").Append(fileName).Append(">").AppendLine("");
+            builder.Append(tab).Append("public class ").Append(fileName).Append(" : XmlData<").Append(fileName).Append(">").Append(", IStream").AppendLine("");
             builder.Append(tab).AppendLine("{");
             int count = nameTypes.Count;
             for (int i = 0; i < count; ++i)
@@ -159,9 +166,84 @@ namespace Nullspace
                 {
                     continue;
                 }
-                builder.Append(doubleTab).Append("public ").Append(nameTypes[i]).Append(" ").Append(names[i]).Append(" { ").Append(" get; set; }").AppendLine("");
+                builder.Append(doubleTab).Append("public ").Append(nameTypes[i]).Append(" ").Append(names[i]).Append(" { get; set; }").AppendLine();
             }
+            builder.AppendLine();
+
+            builder.Append(doubleTab).Append("public int SaveToStream(NullMemoryStream stream)").AppendLine();
+            builder.Append(doubleTab).Append("{").AppendLine();
+            builder.Append(doubleTab).Append(tab).Append("int size = 0;").AppendLine();
+            for (int i = 0; i < count; ++i)
+            {
+                if (removeField.Contains(names[i]))
+                {
+                    continue;
+                }
+                builder.Append(doubleTab).Append(tab).Append("size += ").Append(GetWriteString(names[i], nameTypes[i])).AppendLine();
+            }
+            builder.Append(doubleTab).Append(tab).Append("return size;").AppendLine();
+            builder.Append(doubleTab).Append("}").AppendLine();
+
+            builder.AppendLine();
+            builder.Append(doubleTab).Append("public bool LoadFromStream(NullMemoryStream stream)").AppendLine();
+            builder.Append(doubleTab).Append("{").AppendLine();
+            builder.Append(doubleTab).Append(tab).Append("bool res = true;").AppendLine();
+            for (int i = 0; i < count; ++i)
+            {
+                if (removeField.Contains(names[i]))
+                {
+                    continue;
+                }
+                builder.Append(doubleTab).Append(tab).Append("res &= ").Append(GetReadString(names[i], nameTypes[i])).AppendLine();
+            }
+            builder.Append(doubleTab).Append(tab).Append("return res;").AppendLine();
+            builder.Append(doubleTab).AppendLine("}");
+
             builder.Append(tab).AppendLine("}");
+        }
+
+        public static string GetWriteString(string name, string type)
+        {
+            if (type == "bool")
+            {
+                return string.Format("stream.WriteBool({0})", name);
+            }
+            else if(type == "int")
+            {
+                return string.Format("stream.WriteInt({0})", name);
+            }
+            else if (type == "short")
+            {
+                return string.Format("stream.WriteShort({0})", name);
+            }
+            else if (type == "string")
+            {
+                return string.Format("stream.WriteString({0})", name);
+            }
+            // todo
+            return null;
+        }
+
+        public static string GetReadString(string name, string type)
+        {
+            if (type == "bool")
+            {
+                return string.Format("stream.ReadBool(out {0})", name);
+            }
+            else if (type == "int")
+            {
+                return string.Format("stream.ReadInt(out {0})", name);
+            }
+            else if (type == "short")
+            {
+                return string.Format("stream.ReadShort(out {0})", name);
+            }
+            else if (type == "string")
+            {
+                return string.Format("stream.ReadString(out {0})", name);
+            }
+            // todo
+            return null;
         }
 
         public static void ExcelToXml(string fileFullPath, string sheetName = "Sheet1", int colName = 1, int startDataRow = 4)
@@ -233,5 +315,6 @@ namespace Nullspace
                 }
             }
         }
+
     }
 }
